14ab908bd56bef0d13553ec6f8d5d18a26521ade
tfuxu <73042332+tfuxu@users.noreply.github.com>
2022-05-22 17:34:42 +0200

    Update scripts to work with Pyinstaller
    * Update Github Actions workflows
    * Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 python3 -m venv venv
-. venv/bin/activate
+source venv/bin/activate
 pip install wheel
 pip install -r requirements.txt
+pyinstaller sonix-flasher.spec
 ./dist/sonix-flasher/sonix-flasher
